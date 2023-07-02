ed5f2f1c614990a7a117a355a36fcae6e111b0f2
jamibear <5701ja.mi@gmail.com>
2023-07-02 11:15:43 +0800

    add lsp for svelte filetypes

--- nvim/after/plugin/null.lua
+++ nvim/after/plugin/null.lua
@@ -5,15 +5,25 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.diagnostics.eslint_d.with({
-			extra_filetypes = { "astro" },
+			extra_filetypes = { "astro", "svelte" },
 		}),
 		null_ls.builtins.completion.spell,
 		null_ls.builtins.formatting.prettierd.with({
-			extra_filetypes = { "astro" },
+			extra_filetypes = { "astro", "svelte" },
 		}),
 		null_ls.builtins.diagnostics.write_good,
+
+		null_ls.builtins.diagnostics.golangci_lint, -- golang | https://github.com/golangci/golangci-lint (~/.golangci.yml)
+		null_ls.builtins.diagnostics.staticcheck, -- golang | https://github.com/dominikh/go-tools
+		null_ls.builtins.formatting.fixjson, -- json | https://github.com/rhysd/fixjson
+		null_ls.builtins.formatting.goimports_reviser, -- golang | https://pkg.go.dev/github.com/incu6us/goimports-reviser
+		null_ls.builtins.formatting.markdown_toc, -- markdown | https://github.com/jonschlinkert/markdown-toc
+		null_ls.builtins.formatting.mdformat, -- markdown | https://github.com/executablebooks/mdformat
+		null_ls.builtins.formatting.shfmt, -- shell | https://github.com/mvdan/sh
+		null_ls.builtins.formatting.yamlfmt, -- yaml | https://github.com/google/yamlfmt
+		null_ls.builtins.formatting.taplo, -- toml | https://taplo.tamasfe.dev/
 	},
-	filetypes = { "html", "json", "yaml", "markdown", "astro", "typescript", "javascript", "typescript.tsx" },
+	-- filetypes = { "html", "json", "yaml", "markdown", "astro", "typescript", "javascript", "typescript.tsx" },
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
--- nvim/lua/jami/plugins.lua
+++ nvim/lua/jami/plugins.lua
@@ -1,69 +1,70 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end 
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup({function()
-   use 'wbthomason/packer.nvim'
-   -- use 'Mofiqul/dracula.nvim'
-   -- use 'neanias/everforest-nvim'
-    use 'ellisonleao/gruvbox.nvim'
-	use "norcalli/nvim-colorizer.lua"-- highlighter
-	use "windwp/nvim-autopairs" 		-- auto pair "" () {} 
-	use "junegunn/limelight.vim" 		-- distraction free vim
+return require("packer").startup({
+	function()
+		use("wbthomason/packer.nvim")
+		-- use 'Mofiqul/dracula.nvim'
+		-- use 'neanias/everforest-nvim'
+		use("ellisonleao/gruvbox.nvim")
+		use("norcalli/nvim-colorizer.lua") -- highlighter
+		use("windwp/nvim-autopairs") -- auto pair "" () {}
+		use("junegunn/limelight.vim") -- distraction free vim
 
-    use "nvim-lua/plenary.nvim"
+		-- use("ray-x/go.nvim") too much for now
 
-	use "neovim/nvim-lspconfig" 		-- lsp servers
-	use "hrsh7th/cmp-nvim-lsp" 		-- lsp server completion
-	use "hrsh7th/cmp-buffer" 			-- buffer completion 
-	use "hrsh7th/cmp-path" 				-- path completion 
-	use "hrsh7th/nvim-cmp"  			-- neovim completion 
+		use("nvim-lua/plenary.nvim")
 
-	use "L3MON4D3/LuaSnip" 				-- enable snippet 
+		use("neovim/nvim-lspconfig") -- lsp servers
+		use("hrsh7th/cmp-nvim-lsp") -- lsp server completion
+		use("hrsh7th/cmp-buffer") -- buffer completion
+		use("hrsh7th/cmp-path") -- path completion
+		use("hrsh7th/nvim-cmp") -- neovim completion
 
-	use {"kyazdani42/nvim-tree.lua",
-			requires="kyazdani42/nvim-web-devicons"}
+		use("L3MON4D3/LuaSnip") -- enable snippet
 
-	use "nvim-lualine/lualine.nvim"
-	use "akinsho/bufferline.nvim"
+		use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
 
-	use "nvim-treesitter/nvim-treesitter"
+		use("nvim-lualine/lualine.nvim")
+		use("akinsho/bufferline.nvim")
 
-	use {"nvim-telescope/telescope.nvim",
-			requires="nvim-lua/plenary.nvim"}
-	use "jose-elias-alvarez/null-ls.nvim" -- linter formatter
+		use("nvim-treesitter/nvim-treesitter")
 
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
+		use({ "nvim-telescope/telescope.nvim", requires = "nvim-lua/plenary.nvim" })
+		use("jose-elias-alvarez/null-ls.nvim") -- linter formatter
 
-  use("folke/zen-mode.nvim")
-  use("github/copilot.vim")
-  use("eandrju/cellular-automaton.nvim")
-  use("laytan/cloak.nvim")
+		use("mbbill/undotree")
+		use("tpope/vim-fugitive")
 
-  -- # SYNC TO JUPYTER NOTEBOOK 
-  use { "kiyoon/jupynium.nvim", run = "pip3 install --user ." }
+		use("folke/zen-mode.nvim")
+		use("github/copilot.vim")
+		use("eandrju/cellular-automaton.nvim")
+		use("laytan/cloak.nvim")
 
-    if packer_bootstrap then
-    require('packer').sync()
-    end
-end,
+		-- # SYNC TO JUPYTER NOTEBOOK
+		use({ "kiyoon/jupynium.nvim", run = "pip3 install --user ." })
 
-config = {
-  profile = {
-    enable = true,
-    threshold = 0 -- the amount in ms that a plugins load time must be over for it to be included in the profile
+		if packer_bootstrap then
+			require("packer").sync()
+		end
+	end,
 
-  }
-}})
+	config = {
+		profile = {
+			enable = true,
+			threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+		},
+	},
+})
